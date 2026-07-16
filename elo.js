504ecd68c2c35a6cf145d313b2c3717c816a455b
indie <mail@215.lol>
2026-07-16 22:33:39 +1000

    winrate

--- elo.js
+++ elo.js
@@ -2,6 +2,6 @@ fetch('https://broad-lake-b1fb.indie-the-duck.workers.dev', {
 })
   .then(response => response.json())
   .then(data => {
-    document.body.innerHTML += `<p class="text-xl font-bold text-blue-500">${data.ranks.premier}</p> <p>this data is live! provided by leetify.com</p>`;
+    document.body.innerHTML += `<p class="text-xl font-bold text-blue-500">${data.ranks.premier} (winrate: ${Math.round(data.winrate * 100)}%)</p> <p>this data is live! provided by leetify.com</p>`;
   })
   .catch(error => console.error('Error:', error));
